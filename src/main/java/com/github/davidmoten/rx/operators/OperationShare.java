fcd4ca2a0ad2f23f3099a3acd52af93b1b2f6581
Dave Moten <davidmoten@gmail.com>
2014-03-06 22:13:23 +1100

    add operator

--- src/main/java/com/github/davidmoten/rx/operators/OperationShare.java
+++ src/main/java/com/github/davidmoten/rx/operators/OperationShare.java
@@ -5,6 +5,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 import rx.Observable;
 import rx.Observable.OnSubscribe;
+import rx.Observable.Operator;
 import rx.Subscriber;
 import rx.Subscription;
 import rx.subjects.PublishSubject;
@@ -54,5 +55,49 @@ public class OperationShare {
 				}
 			}
 		}
+	}
+
+	public static class ShareOperator<T> implements Operator<T, T> {
+
+		private final Observable<? extends T> source;
+		private final PublishSubject<T> subject = PublishSubject.create();
+		private final AtomicInteger observersCount = new AtomicInteger(0);
+		private final AtomicReference<Subscription> mainSubscription = new AtomicReference<Subscription>(
+				Subscriptions.empty());
+		private final Object lock = new Object();
+
+		ShareOperator(Observable<? extends T> source) {
+			this.source = source;
+		}
+
+		@Override
+		public Subscriber<? super T> call(final Subscriber<? super T> subscriber) {
+			subject.subscribe(subscriber);
+			synchronized (lock) {
+				if (observersCount.incrementAndGet() == 1) {
+					mainSubscription.set(source.subscribe(subject));
+				}
+			}
+			Subscriber<T> result = new Subscriber<T>() {
+
+				@Override
+				public void onCompleted() {
+					subscriber.onCompleted();
+				}
+
+				@Override
+				public void onError(Throwable e) {
+					subscriber.onError(e);
+				}
+
+				@Override
+				public void onNext(T t) {
+					subscriber.onNext(t);
+				}
+			};
+			subscriber.add(result);
+			return result;
+		}
+
 	}
 }
